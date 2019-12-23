ddc00926b33cca5848f51edbcec9528306263dad
Chayan Martinez <nayachmartinez68@gmail.com>
2019-12-23 05:05:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,6 +56,16 @@ end
 
 sum_array([5,6,7,8,9,20])
 
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
 
+add_s(["ches",
 
   
